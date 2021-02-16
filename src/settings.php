1a7ec117e8e186a37012b22be80a06f19fe6e0cb
LutiGysar <61494551+LutiGysar@users.noreply.github.com>
2021-02-16 22:21:48 +0500

    Add files via upload

---
+++ src/settings.php
@@ -0,0 +1,12 @@
+<?php
+//https://8813.gorodprodazh.ru/index.php
+//https://8813.gorodprodazh.ru/src/install.php
+//define('C_REST_CLIENT_ID','local.5f3858d46bf081.80576773');//Application ID
+//define('C_REST_CLIENT_SECRET','ArILi4xWcxDmrhfRmjztHhT3F3skAXkvwKnUMh0DblbXkRp3Au');//Application key
+// or
+define('C_REST_WEB_HOOK_URL','https://demonstratsionnyy.bitrix24.ru/rest/1/f0lfuaa7s54juu93/');//url on creat Webhook
+//define('C_REST_CURRENT_ENCODING','windows-1251');
+//define('C_REST_IGNORE_SSL',true);//turn off validate ssl by curl
+//define('C_REST_LOG_TYPE_DUMP',true); //logs save var_export for viewing convenience
+//define('C_REST_BLOCK_LOG',true);//turn off default logs
+//define('C_REST_LOGS_DIR', __DIR__ .'/logs/'); //directory path to save the log
